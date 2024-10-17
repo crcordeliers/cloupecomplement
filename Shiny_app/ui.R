459e46d9cc524f645fcec705bc95ccee7f31b363
n-salaun <nicolasalaun@orange.fr>
2024-10-17 10:54:53 +0200

    Update to features
    I tried for several hours to make a folder upload button:
    -ShinyFiles works, but its ugly and archaic, and doesn't go with the rest of the app (also crashes sometoimes for no reason when collapsing folders???)
    -shenanigans with importing files from outs folder, and just getting the path of the folder from that file, BUT for security reasons, fileinput loads the file to tmp/ so cant get the foldername that way.

--- Shiny_app/ui.R
+++ Shiny_app/ui.R
@@ -39,7 +39,7 @@ ui <- dashboardPage(
                      fileInput("cluster_csv", "Choose Cluster CSV File", 
                                accept = ".csv", placeholder = "No file selected")
               )
-            ),
+            )
           )
         ),
         
